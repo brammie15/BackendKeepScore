50fc08b9f9d54d639e0ad2a7f30800dbd6d9a5b5
brammie15 <Brammieverhulst@gmail.com>
2022-12-25 04:06:31 +0100

    Add self Check

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const installRest = require('./rest/index');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+const axios = require('axios');
 const {
     getLogger,
     initializeLogger
@@ -31,7 +32,7 @@ const main = async () => {
         const serverConfig = {
             domain: 'brammie15.dev', // your domain
             https: {
-                port: 5000, // any port that is open and not already used on your server
+                port: 443, // any port that is open and not already used on your server
                 options: {
                     key: fs.readFileSync(path.resolve(process.cwd(), 'certs/privkey.pem'), 'utf8').toString(),
                     cert: fs.readFileSync(path.resolve(process.cwd(), 'certs/fullchain.pem'), 'utf8').toString(),
@@ -61,14 +62,24 @@ const main = async () => {
     if(isDevelopment){
         app.listen(5000);
     }else{
-        const httpServer = https.createServer(serverConfig.https.options, app.callback()).listen(config.https.port);
+        const httpServer = https.createServer(serverConfig.https.options, app.callback());
         httpServer.listen(serverConfig.https.port, () => {
-            logger.info(`HTTPS server listening on port ${config.https.port}`);
+            logger.info(`HTTPS server listening on port ${serverConfig.https.port}`);
+        });
+
+        //test if https works
+        axios.get("https://" + serverConfig.domain).then((res) => {
+            if(res.status === 200){
+                logger.info('HTTPS works');
+            }else{
+                logger.error('HTTPS does not work');
+            }
         });
     }
 
-
-    logger.info(`Server started on port 5000`);
+    if(isDevelopment){
+        logger.info(`Server started on port 5000`);
+    }
 }
 
 
